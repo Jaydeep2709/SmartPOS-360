4e0b2963051ce82d35fa73554f18b9e286424554
Jaydeep Khot <jaydeepkhot2709@gmail.com>
2026-05-25 18:55:13 +0530

    Created Temp Data and Service for Login

---
+++ frontend/src/app/core/data/data.ts
@@ -0,0 +1,12 @@
+export const USERS = [
+  {
+    email: 'admin@smartpos.com',
+    password: 'Admin@123',
+    role: 'Admin'
+  },
+  {
+    email: 'cashier@smartpos.com',
+    password: 'Cashier@123',
+    role: 'Cashier'
+  }
+];
--- frontend/src/app/core/services/auth.ts
+++ frontend/src/app/core/services/auth.ts
@@ -1,8 +1,53 @@
 import { Injectable } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { USERS } from '../data/data';
 @Injectable({
   providedIn: 'root',
 })
 export class Auth {
   
+  constructor(private router: Router) {}
+
+  login(email: string, password: string, rememberMe: boolean): boolean {
+
+    const user = USERS.find(
+      x => x.email === email && x.password === password
+    );
+
+    if (user) {
+
+      localStorage.setItem('isLoggedIn', 'true');
+
+      if (rememberMe) {
+        localStorage.setItem('rememberedEmail', email);
+      } else {
+        localStorage.removeItem('rememberedEmail');
+      }
+
+      localStorage.setItem('currentUser', JSON.stringify(user));
+
+      return true;
+    }
+
+    return false;
+  }
+
+  logout() {
+
+    localStorage.removeItem('isLoggedIn');
+    localStorage.removeItem('currentUser');
+
+    this.router.navigate(['/login']);
+  }
+
+  getRememberedEmail(): string {
+
+    return localStorage.getItem('rememberedEmail') || '';
+  }
+
+  isLoggedIn(): boolean {
+
+    return localStorage.getItem('isLoggedIn') === 'true';
+  }
+
 }
