ae3d74cba5674513db1b484a4ad9c54720a5b306
Jaydeep Khot <jaydeepkhot2709@gmail.com>
2026-05-25 18:58:31 +0530

    Add Route and SubRoute for Auth Login and Dashbord

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import {Login} from './features/auth/login/login'
+import { AuthLayout } from './layouts/auth-layout/auth-layout';
+import { Dashboard } from './features/dashboard/dashboard/dashboard';
+export const routes: Routes = [
 
-export const routes: Routes = [];
+ {
+    path: '',
+    component: AuthLayout,
+    children: [
+
+      {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+      },
+
+      {
+        path: 'login',
+        component: Login
+      }
+
+    ],
+  },
+  {
+    path: 'dashboard',
+    component: Dashboard,
+
+  }
+
+];
