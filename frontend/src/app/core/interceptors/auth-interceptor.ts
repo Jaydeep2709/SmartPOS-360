a4bb2f8d076ae201f546bdfbd6e8c9bc641baa5b
Jaydeep Khot <jaydeepkhot2709@gmail.com>
2026-05-27 14:56:55 +0530

    interceptor to add token in to header

--- frontend/src/app/core/interceptors/auth-interceptor.ts
+++ frontend/src/app/core/interceptors/auth-interceptor.ts
@@ -1,5 +1,29 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import {
+  HttpInterceptorFn
+} from '@angular/common/http';
 
-export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
+export const authInterceptor:
+HttpInterceptorFn = (
+
+  req,
+  next
+
+) => {
+
+  const token =
+    localStorage.getItem('token');
+
+  const authReq = req.clone({
+
+    setHeaders: {
+
+      Authorization:
+        `Bearer ${token}`
+
+    }
+
+  });
+
+  return next(authReq);
+
 };
